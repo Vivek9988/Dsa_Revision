1643185e57721f7b69c12711f2592105ef691b7e
Vivek9988 <vivek19102004@gmail.com>
2024-08-14 01:40:53 +0530

    new code

---
+++ Sorting/Intertion.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+public class Intertion {
+
+    public static void insertionSort(int arr[]){
+        for (int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                
+            }
+            
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,1,4,3,2};
+    }
+    
+}
