127e70743072186a0d1a5b242f9fb1ac0808167b
Vivek9988 <vivek19102004@gmail.com>
2024-08-18 11:37:05 +0530

    changes new code

--- Sorting/Bubble.java
+++ Sorting/Bubble.java
@@ -3,15 +3,22 @@ package Sorting;
 public class Bubble {
 
     public static void bubbleSort(int arr[]){
+        Boolean val = true;
         for (int i=0;i<arr.length-1;i++){
-            
+
             for (int j = 0; j < arr.length - 1 - i; j++) {
 
                 if (arr[j] > arr[j + 1]) {
+                    val =false;
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+                
+            }
+            if (val) {
+                System.out.println("aray is sorted");
+                break;
             }
         }
     }
@@ -22,7 +29,7 @@ public class Bubble {
     }
 
     public static void main(String args[]){
-        int arr[]={5,4,1,3,2};
+        int arr[]={1,2,3,4,5};
         bubbleSort(arr);
         printArr(arr);
     }
---
+++ Sorting/Inbuilt.java
@@ -0,0 +1,24 @@
+package Sorting;
+import java.util.*;
+
+public class Inbuilt {
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,4,2,3,1};
+       //Arrays.sort(arr);
+       //printArr(arr);
+
+       for (int i=0;i<1;i++){
+        System.out.print(i + " ");
+       }
+
+    }
+    
+}
