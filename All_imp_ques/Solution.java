9dec585d87e6d65fabc65a7f485ac167f80115c8
Vivek9988 <vivek19102004@gmail.com>
2024-09-02 16:16:11 +0530

    new code

---
+++ All_imp_ques/Solution.java
@@ -0,0 +1,15 @@
+package All_imp_ques;
+
+public class Solution {
+    public static void main (String args[]){
+        int arrr[]={10};
+        int arr []= new int[10];
+        System.out.println(arr[1]);
+        int a = 9;
+        a=8;
+        System.out.println(a);
+        int [] viv;//
+        viv=new int [9];
+    }
+    
+}
