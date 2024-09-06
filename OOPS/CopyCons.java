82f3e4e1a5cfcb3008fbe58f1e091ec38d51e329
Vivek9988 <vivek19102004@gmail.com>
2024-09-06 11:42:43 +0530

    new code

--- OOPS/CopyCons.java
+++ OOPS/CopyCons.java
@@ -6,10 +6,18 @@ public class CopyCons {
         s1.name = "vivek";
         s1.roll = 34;
         s1.password = "abcd";
+        s1.marks[0]=1;
+        s1.marks[1]=2;
+        s1.marks[2]=3;
+        
 
         Student s2 = new Student(s1);
         s2.password = "cdf";
+        s1.marks[2]=5;                         //means it is call by reference ;
         System.out.println(s2.password);
+        for (int i=0;i<3;i++){
+            System.out.println(s2.marks[i]);
+        }
     }
 }
 
@@ -17,12 +25,16 @@ class Student {
     String name;
     int roll;
     String password;
+    int marks[]=new int[3];
 
     // Copy Constructor
     Student(Student s1) {
+      //  marks= new int[3];
         this.roll = s1.roll;
         this.name = s1.name;
         this.password = s1.password;     //if we will not pass also the program will run
+        this.marks=s1.marks;
+         
     }
 
     // Default Constructor
